070ebbb09c3818ec2c903b5a658ed1b8e90abdc9
nycodev <nycolas_borowski@hotmail.com>
2021-11-10 10:43:46 -0400

    primeiro teste em prod

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,7 +2,8 @@ function tempo(request, response) {
   const dynamicDate = new Date();
 
   response.json({
-      date: dynamicDate.toGMTString()
+      date: dynamicDate.toGMTString(),
+      phrase: 'bro, a API tá deployed!'
   })
 }
 
